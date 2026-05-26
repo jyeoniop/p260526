2c788288962e38d2759da8c96f269f34e41beb73
jyeoniop <wldud128iop@gmail.com>
2026-05-26 14:28:09 +0900

    client pord & test

---
+++ src/main/kotlin/com/example/back2/HomeController.kt
@@ -0,0 +1,24 @@
+package com.example.back2
+
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.services.s3.S3Client
+
+
+@RestController
+class HomeController(
+    private val s3Service: S3Service
+) {
+
+    @GetMapping
+    fun main(): String {
+        return "Hi"
+    }
+
+    @GetMapping("/buckets")
+    fun buckets(): List<String> {
+        return s3Service.getBucketNames()
+    }
+
+}
---
+++ src/main/kotlin/com/example/back2/S3Service.kt
@@ -0,0 +1,5 @@
+package com.example.back2
+
+interface S3Service {
+    fun getBucketNames(): List<String>
+}
